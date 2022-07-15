4b0633b6bd5a0a3eb1392aaa69a8918f41388ed3
Aztim <barboza-82@mail.ru>
2022-07-15 23:52:39 +0300

    created LoginForm -component

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -48,30 +48,13 @@
           </ul>
 
           <!-- Login Form -->
-          <form v-show="tab === 'login'">
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <input type="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email" />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <input type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" />
-            </div>
-            <button type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
-                hover:bg-purple-700">
-              Submit
-            </button>
-          </form>
+
           <!-- Registration Form -->
+          <div class="text-white text-center font-bold p-5 mb-4"
+            v-if="reg_show_alert" :class="reg_alert_variant"
+          >
+            {{ reg_alert_msg }}
+          </div>
           <vee-form v-show="tab === 'register'" :validation-schema="schema"
             @submit="register" :initial-values="userData">
             <!-- Name -->
@@ -142,7 +125,7 @@
               <label class="inline-block">Accept terms of service</label>
               <ErrorMessage class="text-red-600 block" name="tos" />
             </div>
-            <button type="submit"
+            <button type="submit" :disable="reg_in_submission"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
                 hover:bg-purple-700">
               Submit
@@ -174,6 +157,14 @@ export default {
       userData: {
         country: 'USA',
       },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! You account is being created.',
+      loginSchema: {
+        email: 'required|email',
+        password: 'required|min:3|max:32',
+      },
     };
   },
   computed: {
@@ -184,7 +175,17 @@ export default {
   },
   methods: {
     ...mapMutations(['toggleAuthModal']),
+    // eslint-disable-next-line no-unused-vars
     register(values) {
+      this.reg_show_alert = true;
+      this.reg_in_submission = true;
+      this.reg_alert_variant = 'bg-blue-500';
+      this.reg_alert_msg = 'Please wait! You account is beong created.';
+
+      this.reg_alert_variant = 'bg-green-500';
+      this.reg_alert_msg = 'Success! You account has been created.';
+    },
+    login(values) {
       console.log(values);
     },
   },
